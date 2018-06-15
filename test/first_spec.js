dfb27f347a699404b5668f75430d924b42b4df9e
Maxmi <gulmira2410@gmail.com>
2018-06-15 13:29:23 -0700

    initial commit

---
+++ test/first_spec.js
@@ -0,0 +1,10 @@
+var assert = require('assert')
+
+describe('a feature', () => {
+  
+  describe('a scenarion', () => {
+    it('does something expected', () => {
+      assert(true);      
+    });
+  });
+});
